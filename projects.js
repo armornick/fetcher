159520d9b7c7b39bffd9f54b7901e4fa671369f1
armornick <nick.arnoeyts@mail.com>
2020-02-11 10:44:09 +0100

    add project packages

--- projects.js
+++ projects.js
@@ -1,7 +1,38 @@
 module.exports = {
 	
-	defaults: ['reactApp'],
+	defaults: ['vdom-pkg'],
 	isolate: true,
+
+	// package bundles
+
+	"browserify-pkg": {
+		subs: ['browserify-bundle','less-bundle','stylus-bundle','babel-bundle','buble-bundle','ejs-bundle','pug-bundle','swig-bundle']
+	},
+
+	"webpack-pkg": {
+		subs: ['webpack-bundle','less-bundle','sass-bundle','stylus-bundle','postcss-bundle','babel-bundle','buble-bundle',
+			'ejs-bundle','pug-bundle','swig-bundle']
+	},
+
+	"libraries-pkg": {
+		subs: ['popular-libraries','server-libraries','microjs-bundle','vdom-libraries','template-libraries']
+	},
+
+	"templates-pkg": {
+		subs: ['template-libraries','mustache-bundle','ejs-bundle','pug-bundle','handlebars-bundle','swig-bundle']
+	},
+
+	"vue-pkg": {
+		subs: ['vue-app','vue-bundle']
+	},
+
+	"react-pkg": {
+		subs: ['reactApp','react-bundle','babel7-react-bundle','buble-bundle']
+	},
+
+	"vdom-pkg": {
+		subs: ['preactApp','riot-bundle','web-components','vdom-libraries']
+	},
 
 	// build tool packages
 
@@ -30,6 +61,11 @@ module.exports = {
 
 	"misc-libs": {
 		packages: ['lunr','form-to-obj','flexsearch','director','asciidoctor.js']
+	},
+
+	"babel-singles": {
+		subs: ['babel7-preset-env','babel7-preset-bundle','babel7-react-bundle',
+			'babel6-preset-env','babel6-preset-bundle','babel6-react-bundle']
 	},
 
 }
